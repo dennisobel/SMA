64aa4c7e94fdf0057474b0747e20eb2f6d5b9e01
Dennis Obel <ogembodenis2016@gmail.com>
2017-08-14 07:08:32 +0300

    5th commit

--- src/pages/rates/rates.ts
+++ src/pages/rates/rates.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { ViewController, NavParams, ModalController } from 'ionic-angular';
 import { ListPage } from "../list/list"
-//import dummy from "../../data/dummydata";
+import dummy from "../../data/dummydata";
 
 @Component({
   selector: 'page-rates',
   templateUrl: 'rates.html',
 })
-export class RatesPage {
-record: any[]=[]
+export class RatesPage { 
 
-//dummyData: any
+
+record: any[]=[]
+dummyData: any
+amnt:any;
+from:any[]=[];
+originctry:any;    
 name: string;
 flag: string;
 amount: any[]=[]
@@ -22,11 +26,28 @@ ionViewDidLoad() {
     this.name = this.navParams.get("name")
     this.flag = this.navParams.get("flag")    
     this.amount = this.navParams.get("amount")
+    
+    this.dummyData = dummy
+    for(var i = 0; i < this.dummyData.length; i++){
+        this.from = this.dummyData[i].from
+        console.log(this.from)
+        
+        
+        
+        for(var j = 0; j < this.from.length; j++){
+            this.originctry = this.from[j]
+            //console.log(this.originctry)
+        }
+        
+    }
+    
 }
 
 onClick(amt){    
     this.record = amt.record
     console.log(this.record)
+    this.amnt = amt.amount
+    console.log(this.amnt)
 }
     
 onClose(remove = false){
